8a754612fb82f26805af49f87ed4f552a80f6832
Bergrin Yuriy <yubergrin@fil-it.ru>
2020-04-14 19:02:21 +0300

    add tests

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -1,22 +1,27 @@
 package constants;
 
+import static constants.Constants.Servers.JSON_PLACEHOLDER_URL;
 import static constants.Constants.Servers.REQRES_URL;
-import static constants.Constants.Path.REQRES_PATH;
+import static constants.Constants.Servers.REQUEST_BIN_URL;
+import static constants.Constants.Path.JSON_PLACEHOLDER_PATH;
 
 public class Constants {
 
     public static class RunVariable {
-        public static String server = REQRES_URL;
-        public static String path = REQRES_PATH;
+        public static String server = JSON_PLACEHOLDER_URL;
+        public static String path = "";
     }
 
     public static class Servers {
         public static String REQRES_URL = "https://reqres.in/";
+        public static String JSON_PLACEHOLDER_URL = "https://jsonplaceholder.typicode.com/";
+        public static String REQUEST_BIN_URL = "https://enqhtc12qo59.x.pipedream.net";
         public static String GOOGLE_PLACES_URL;
     }
 
     public static class Path {
         public static String REQRES_PATH = "api/";
+        public static String JSON_PLACEHOLDER_PATH = "";
         public static String GOOGLE_PLACES_PATH;
     }
 
@@ -28,6 +33,13 @@ public class Constants {
         /**
          * google places
          */
-        public static String GOOGLE_PLACES_PATH;
+
+        /**
+         * json placeholder
+         */
+        public static String JSON_PLACEHOLDER_GET_POSTS = "comments/";
+        public static String JSON_PLACEHOLDER_PUT_POST = "posts/1";
+        public static String JSON_PLACEHOLDER_POST_DELETE = "posts/1";
+        public static String JSON_PLACEHOLDER_POST_POST = "posts/";
     }
 }
--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,14 +1,72 @@
 import config.TestConfig;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
+import java.util.Map;
+
 import static constants.Constants.Actions.REQRES_GET_USERS;
+import static constants.Constants.Path.REQRES_PATH;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
 
 public class FirstTest extends TestConfig {
     @Test
     public void myFirstTest() {
-    given().
-            when().get(REQRES_GET_USERS + "1").
-            then().statusCode(200);
+        given().log().uri().
+                when().get(REQRES_GET_USERS + "1").
+                then().log().body().statusCode(200);
+    }
+
+    @Test
+    public void getSomeFieldInResponseAssertion() {
+        given().spec(requestSpecificationForReqres).log().uri().
+                when().get(REQRES_PATH + REQRES_GET_USERS).
+                then().body("page", equalTo(1)).log().body();
+    }
+
+    @Test
+    public void getSomeFieldInResponseWithIndexAssertion() {
+        given().spec(requestSpecificationForReqres).log().uri().
+                when().get(REQRES_PATH + REQRES_GET_USERS).
+                then().body("total", equalTo(12)).
+                body("data.first_name[0]", equalTo("George")).
+                log().body();
+
+    }
+
+    @Test
+    public void getAllDataFromResponse() {
+        Response response = given().spec(requestSpecificationForReqres).log().uri().
+                when().get(REQRES_PATH + REQRES_GET_USERS).
+                then().extract().response();
+
+        String jsonResponseAsString = response.asString();
+        System.out.println(jsonResponseAsString);
+    }
+
+    @Test
+    public void getCookiesFromResponse() {
+        Response response = given().spec(requestSpecificationForReqres).log().uri().
+                when().get(REQRES_PATH + REQRES_GET_USERS).
+                then().extract().response();
+        Map<String, String> allCookies = response.getCookies();
+        System.out.println("All cookies --> " + allCookies);
+
+        String someCookie = response.getCookie("__cfduid");
+        System.out.println("someCookie --> " + someCookie);
+    }
+
+    @Test
+    public void getHeadersFromResponse() {
+        Response response = given().spec(requestSpecificationForReqres).log().uri().
+                when().get(REQRES_PATH + REQRES_GET_USERS).
+                then().extract().response();
+
+        Headers headers = response.getHeaders();
+        System.out.println("Headers -->\n " + headers);
+
+        String contentType = response.getContentType();
+        System.out.println("contentType-->\n" + contentType);
     }
 }
